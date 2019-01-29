7b6bc3b0b8a25c6417eeb2b6487507c474e3a081
rufin0615 <d.drakuwa@gmail.com>
2019-01-29 23:11:36 +0900

    Locate components on main screen

---
+++ MyHousekeepingBook/MyHousekeepingBook/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MyHousekeepingBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.mainMenue = new System.Windows.Forms.MenuStrip();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 40);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 21;
+            this.dgv.Size = new System.Drawing.Size(776, 340);
+            this.dgv.TabIndex = 0;
+            // 
+            // mainMenue
+            // 
+            this.mainMenue.Location = new System.Drawing.Point(0, 0);
+            this.mainMenue.Name = "mainMenue";
+            this.mainMenue.Size = new System.Drawing.Size(800, 24);
+            this.mainMenue.TabIndex = 1;
+            this.mainMenue.Text = "menuStrip1";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(13, 399);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(103, 26);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "追加";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(136, 399);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(103, 26);
+            this.buttonChange.TabIndex = 2;
+            this.buttonChange.Text = "追加";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(263, 399);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(103, 26);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "削除";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Location = new System.Drawing.Point(685, 399);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(103, 26);
+            this.buttonEnd.TabIndex = 2;
+            this.buttonEnd.Text = "終了";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.mainMenue);
+            this.Name = "Form1";
+            this.Text = "簡易家計簿";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.MenuStrip mainMenue;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEnd;
+    }
+}
+
